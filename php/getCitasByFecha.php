fce2fd9a9f57aec9a4db2a5e781cfc9bc6366384
Julio Hernandez <sistemas.p@mepiel.com.mx>
2020-12-15 17:27:25 -0600

    funcion get citas all, and citas by fecha

--- php/getCitasByFecha.php
+++ php/getCitasByFecha.php
@@ -24,12 +24,15 @@
 		 die( print_r( sqlsrv_errors(), true));
 	}
 
+	$fecha = $_POST['fecha'];
+
 	$query = 
 		"SELECT citas.id, users.nombres, users.apellidos, servicios.descripcion AS 'servicio', citas.fecha, citas.hora, estatus.descripcion AS 'status' 
 		FROM citas
 		INNER JOIN users ON citas.iduser = users.id 
 		INNER JOIN servicios ON citas.idservicio = servicios.id 
-		INNER JOIN estatus ON citas.idestatus = estatus.id";
+		INNER JOIN estatus ON citas.idestatus = estatus.id
+		WHERE citas.fecha = '$fecha'";
 
 	$result = mysqli_query($conn,$query);
 
@@ -51,7 +54,7 @@
 	}
 
 	$conn->close();
-	header('Content-Type: application/json');
+	header('Content-Type: application/json'); 
 
 	echo json_encode($table);
 
