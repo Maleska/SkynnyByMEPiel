a3bfc6f9dbdf504a523de7297a9394b8ed8fac67
Julio Hernandez <sistemas.p@mepiel.com.mx>
2021-01-07 15:15:45 -0600

    hot fix

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -624,6 +624,7 @@ function actualizarEvento(){
 				if(datos === "REGISTRO ACTUALIZADO CORRECTAMENTE"){
 					//alert("");
 				$("#btnidCerrar").click();
+				getCitasByFecha();
                 }
    			 }
 				
